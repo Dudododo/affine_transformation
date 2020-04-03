8597aaa8bfe1ed984e13c0a10729f5e42fbc3961
Dudododo <a752601260@gmail.com>
2020-04-03 14:35:09 +0800

    添加项目文件。

---
+++ affine_transformation.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cv::Mat srcMat = imread("1.jpg", 1);
+	cv::Mat dstMat;
+
+	//变换前的三点坐标
+	const cv::Point2f src_pt[] = {
+		cv::Point2f(200,200),
+		cv::Point2f(250,200),
+		cv::Point2f(200,150)
+	};
+	//变换后的三点坐标
+	const cv::Point2f dst_pt[] = {
+		cv::Point2f(350,150),
+		cv::Point2f(300,50),
+		cv::Point2f(200,100)
+	};
+	//计算仿射矩阵
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	cv::imshow("srcMat", srcMat);
+	cv::imshow("dstMat", dstMat);
+	waitKey(0);
+}
